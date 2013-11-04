d15251ac02e48ac9994ad084f26a65386e78827a
Nicholas Wargnier <nwargnier@gmail.com>
2013-11-04 13:33:40 -0800

    Initial Commit

---
+++ Task/NWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NWAppDelegate.h
+//  Task
+//
+//  Created by Nicholas Wargnier on 11/4/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
